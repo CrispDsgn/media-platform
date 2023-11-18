e9a5da7f14fb98d0dadf65332ea6915275f432f0
cristian-sas <cristian.popa@sas-technologies.ro>
2023-11-18 19:28:44 +0200

    add global theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      test
+      <ThemeToggle />
     </main>
   );
 }
